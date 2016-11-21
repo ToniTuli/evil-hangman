9c82f1ff4d99fa1385cd64595f4ad47869f19ce1
Meriton Tuli <meritoni_9@hotmail.com>
2016-11-21 00:56:27 -0500

    Updated main.c

--- main.c
+++ main.c
@@ -9,10 +9,13 @@ int main(int argc, char* argv[])
 	char c;
 	
 	hMy_string = my_string_init_c_string("Hello this is sprta");
+	hMy_string1 = my_string_init_c_string(" and we are...");
 
 	//hMy_string1 = my_string_init_c_string("Hello");
 
 	my_string_insertion(hMy_string, stdout);
+	printf("\n---\n");
+	my_string_insertion(hMy_string1, stdout);
 	printf("\n---\n");
 	my_string_push_back(hMy_string, 'A');
 	my_string_insertion(hMy_string, stdout);
